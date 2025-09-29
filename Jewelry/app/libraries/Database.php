b66f5ae13c8952668ab742d32841e239c51327b2
Aziz Harkati <104675897+AzizHr@users.noreply.github.com>
2025-09-29 22:54:48 +0100

    Refactor Database.php

--- Jewelry/app/libraries/Database.php
+++ Jewelry/app/libraries/Database.php
@@ -1,83 +1,57 @@
 <?php
-  /*
-   * PDO Database Class
-   * Connect to database
-   * Create prepared statements
-   * Bind values
-   * Return rows and results
-   */
+
   class Database {
+    
     private $host = DB_HOST;
     private $user = DB_USER;
     private $pass = DB_PASS;
     private $dbname = DB_NAME;
 
-    private $dbh;
-    private $stmt;
+    private $connection;
+    private $statement;
     private $error;
 
-    public function __construct(){
-      // Set DSN
+    public function __construct()
+    {
+
       $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
       $options = array(
         PDO::ATTR_PERSISTENT => true,
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
       );
 
-      // Create PDO instance
       try{
-        $this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+        $this-$connection = new PDO($dsn, $this->user, $this->pass, $options);
       } catch(PDOException $e){
         $this->error = $e->getMessage();
         echo $this->error;
       }
     }
 
-    // Prepare statement with query
-    public function query($sql){
-      $this->stmt = $this->dbh->prepare($sql);
+    public function query($sql)
+    {
+      $this->statement = $this-$connection->prepare($sql);
     }
 
-    // Bind values
-    public function bind($param, $value, $type = null){
-      if(is_null($type)){
-        switch(true){
-          case is_int($value):
-            $type = PDO::PARAM_INT;
-            break;
-          case is_bool($value):
-            $type = PDO::PARAM_BOOL;
-            break;
-          case is_null($value):
-            $type = PDO::PARAM_NULL;
-            break;
-          default:
-            $type = PDO::PARAM_STR;
-        }
-      }
-
-      $this->stmt->bindValue($param, $value, $type);
+    public function execute(array $data = [])
+    {
+      return $this->statement->execute($data);
     }
 
-    // Execute the prepared statement
-    public function execute(){
-      return $this->stmt->execute();
-    }
-
-    // Get result set as array of objects
-    public function resultSet(){
+    public function resultSet()
+    {
       $this->execute();
-      return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
+      return $this->statement->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    // Get single record as object
-    public function single(){
+    public function single()
+    {
       $this->execute();
-      return $this->stmt->fetch(PDO::FETCH_ASSOC);
+      return $this->statement->fetch(PDO::FETCH_ASSOC);
     }
 
-    // Get row count
-    public function rowCount(){
-      return $this->stmt->rowCount();
+    public function count()
+    {
+      return $this->statement->rowCount();
     }
   }
